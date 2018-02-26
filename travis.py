0ee35b16d0f951df5c141d8f798cce91fc5190ce
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-26 07:52:44 -0800

    adding new user to the list if confims

--- travis.py
+++ travis.py
@@ -3,13 +3,18 @@ print(len(known_users))
 
 while True:
 	print('Hi, My name is Travis!')
-	name = input('What is your name? ').strip()
+	name = input('What is your name? ').strip().capitalize()
 
 	if name in known_users:
 		print('Hello {}!'.format(name))
-		remove = input('Would you like to be removed from the system (y/n)?: ').lower()
+		remove = input('Would you like to be removed from the system (y/n)?: ').strip().lower()
 
 		if remove == 'y':
-			print(known_users)
 			known_users.remove(name)
-			print(known_users)
+
+	else:
+		print('Hmmm, I dont thing Ive met you yet, {}.'.format(name))
+		add_me = input('Would you like to be added to the system (y/n)?: ').strip().lower()
+
+		if add_me == 'y':
+			known_users.append(name)
