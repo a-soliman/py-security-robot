8f03ec746be13bce5e2cceb08c5bfe19969d18d6
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-26 07:55:16 -0800

    elseif no

--- travis.py
+++ travis.py
@@ -12,9 +12,15 @@ while True:
 		if remove == 'y':
 			known_users.remove(name)
 
+		elif remove == 'n':
+			print('No problem, I didnt want to you to leave anyway.')
+
 	else:
 		print('Hmmm, I dont thing Ive met you yet, {}.'.format(name))
 		add_me = input('Would you like to be added to the system (y/n)?: ').strip().lower()
 
 		if add_me == 'y':
 			known_users.append(name)
+
+		elif add_me == 'n':
+			print('No worries, Have a good day.')
