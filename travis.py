78b6b1c7586f3f548ce13bc41369df2e116b6f6b
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-26 07:45:23 -0800

    chinking if the user exists in the list

--- travis.py
+++ travis.py
@@ -3,4 +3,8 @@ print(len(known_users))
 
 while True:
 	print('Hi, My name is Travis!')
-	
+	name = input('What is your name? ').strip()
+
+	if name in known_users:
+		print('Hello {}!'.format(name))
+		
